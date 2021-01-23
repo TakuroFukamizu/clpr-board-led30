3f4c7d3fb0030cdbaab3e0e9b6702194f3f6d203
TAKURO FUKAMIZU <takuro.f.1987@gmail.com>
2021-01-24 01:52:46 +0900

    fix raindow-basic

--- rainbow-basic/rainbow-basic.ino
+++ rainbow-basic/rainbow-basic.ino
@@ -9,25 +9,28 @@
 9: EXT IO: G19 / SPI MISO
 10: EXT IO: G23 / SPI MOSI
  */
-
 #define PIN_LEDS 27
 #define NUM_LEDS 30
 
 #define BRIGHTNESS 128   /* Control the brightness of your leds */
-#define SATURATION 255   /* Control the saturation of your leds */
+#define SATURATION 128   /* Control the saturation of your leds */
 
 CRGB leds[NUM_LEDS];
 
 void setup() { 
-    FastLED.addLeds<NEOPIXEL, PIN_LEDS>(leds, NUM_LEDS); 
+  Serial.begin(115200);
+  FastLED.addLeds<NEOPIXEL, PIN_LEDS>(leds, NUM_LEDS); 
 }
 
 void loop() {
-    for (int j = 0; j < 255; j++) {
-        for (int i = 0; i < NUM_LEDS; i++) {
-            leds[i] = CHSV(i - (j * 2), BRIGHTNESS, SATURATION); /* The higher the value 4 the less fade there is and vice versa */ 
-        }
-        FastLED.show();
-        delay(50); /* Change this to your hearts desire, the lower the value the faster your colors move (and vice versa) */
+  Serial.println("loop st");
+  for (int j = 0; j < 255; j++) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CHSV(i - (j * 2), BRIGHTNESS, SATURATION); /* The higher the value 4 the less fade there is and vice versa */ 
     }
+    Serial.println("led show");
+    FastLED.show();
+    delay(50); /* Change this to your hearts desire, the lower the value the faster your colors move (and vice versa) */
+  }
+  Serial.println("loop ed");
 }
